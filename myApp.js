3c54f6d3af8efbd636fb79d696011d3f45cc5eca
DavitKhetaguri <khetaguri.david@gmail.com>
2021-02-16 03:30:01 +0400

    initial commit

--- myApp.js
+++ myApp.js
@@ -7,32 +7,40 @@ var app = express();
 
 
 
+/*
+var express = require('express');
+var app = express();
+
+
+app.use(function(req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
+
+
+console.log("Hello World!");
+let absolutePath = __dirname + "/views/index.html";
+
+
+app.get("/", (rq, res) => { res.sendFile(absolutePath); })
+
+
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
 
 
+app.get('/json', (req, res) => {
+  process.env.MESSAGE_STYLE === 'uppercase' ?
+    res.json({ "message": "HELLO JSON" }) :
+    res.json({ "message": "Hello json" })
+});
+module.exports = app;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 
